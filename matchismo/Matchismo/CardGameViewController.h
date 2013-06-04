1459679dcfb705cc54712b82a0d3fbd5a2d22e0e
nickkov89 <nickkov89@gmail.com>
2013-06-03 22:43:23 -0400

    initial matchismo

---
+++ matchismo/Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by nik kov on 6/3/13.
+//  Copyright (c) 2013 Stanford Uni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
